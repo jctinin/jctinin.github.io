70a954d31481db3e9065c1f6da9dc5287febf463
Julio Cesar <stininmatic@hotmail.com>
2020-03-28 19:01:41 -0300

    Exercícios de for

---
+++ exercises/4_2/exercicio2.js
@@ -0,0 +1,9 @@
+let n = 5
+let simbolo="*";
+let linha="";
+
+for(let i=0; i<n; i++){
+    linha+=simbolo;
+    console.log(linha)
+
+}
---
+++ exercises/4_2/exercicio3.js
@@ -0,0 +1,29 @@
+let n = 5
+let simbolo="*";
+let linha="";
+let e=n;
+let espaco="-";
+/*
+for(let i=0; i<n; i++){
+ console.log(espaco);
+} */
+
+
+for(let j=0; j<n; j++){
+    if(j==e-1){
+        e-=1;
+        linha+=simbolo;
+        console.log(linha);
+}else{
+{
+console.log(espaco);
+}
+}
+}
+
+
+
+/*
+for(let j=0; j<n; j++){
+}
+console.log(simbolo) */
