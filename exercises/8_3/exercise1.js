4267cd0cdc46ef13c5f227668d4c8aec35a39fba
Julio Cesar <stininmatic@hotmail.com>
2020-05-05 19:15:45 -0300

    Iniciando os exercícios

---
+++ exercises/8_3/exercise1.js
@@ -0,0 +1,12 @@
+const assert = require('assert');
+
+const soma = (a,b) => {
+    if(typeof a !== 'number' || typeof b !== 'number'){
+        throw new Error('Parameters must be numbers');
+    }
+    return a+b;
+}
+
+assert.equal(soma(4,5), 9, 'O resultado não é o esperado');
+assert.equal(soma(0,0), 0, 'o retorno deve ser 0');
+assert.deepStrictEqual(soma(4,'5'), 'Parameters must be numbers', 'OK');
